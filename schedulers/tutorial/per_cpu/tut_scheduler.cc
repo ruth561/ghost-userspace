4a8b7f5319f7d53b6dd48255c992f5e7c6718a77
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2024-02-27 13:27:22 +0900

    SMPスケジューラを実装した
    この実装によって、CPU1やCPU3にもきちんとタスクを割り振って複数のCPUで
    タスクをスケジューリングできるようになった。タスクの割り振り処理（
    マイグレーション処理）はTaskNewの内部でのみ行うという非常に簡易的な
    実装になっている。

--- schedulers/tutorial/per_cpu/tut_scheduler.cc
+++ schedulers/tutorial/per_cpu/tut_scheduler.cc
@@ -1,9 +1,13 @@
 #include "schedulers/tutorial/per_cpu/tut_scheduler.h"
+#include "lib/ghost.h"
+#include <cstdio>
 
 
 namespace ghost {
 
 void TutorialScheduler::Schedule(const Cpu &cpu, const StatusWord &agent_sw) {
+  auto &cs = cpu_state(cpu);
+
   // ① メッセージの処理をする前に barrier の値を読み出しておく。
   BarrierToken barrier = agent_sw.barrier();
 
@@ -11,9 +15,9 @@ void TutorialScheduler::Schedule(const Cpu &cpu, const StatusWord &agent_sw) {
   // 　 agent に関連付けられた Channel からメッセージを取り出していく。
   // 　 DispatchMessage の内部でメッセージごとの処理が呼び出される。
   Message msg;
-  while (!(msg = channels_[cpu.id()]->Peek()).empty()) {
+  while (!(msg = cs.channel->Peek()).empty()) {
     DispatchMessage(msg);
-    Consume(channels_[cpu.id()].get(), msg);
+    Consume(cs.channel.get(), msg);
   }
   
   // ③ スケジューリング
@@ -28,14 +32,19 @@ void TutorialScheduler::Schedule(const Cpu &cpu, const StatusWord &agent_sw) {
 
   // nextには次に実行状態にするタスクへのポインタを格納する
   TutorialTask *next = nullptr;
-  // ◎ メッセージ処理のときにTaskNew()メンバ関数などでrq_にタスクが
-  //  　プッシュされているはず
-  if (!rq_.empty()) {
-    next = rq_.front();
+
+  // cs.currentがnullptrでなければ、そのタスクの実行を続ける。
+  // cs.currentがnullptrだったときは、cs.rqの先頭のタスクを実行状態にする。
+  if (cs.current) {
+    next = cs.current;
+  } else {
+    next = cs.rq.PopFront();
   }
 
   if (next) { 
-    CHECK(!next->blocked());
+    // nextの状態はkQueuedではあるが、cs.rqには含まれていない状態
+    CHECK(!cs.rq.Has(next));
+    CHECK(next->queued());
 
     // next候補があればトランザクションを用意
     req->Open({
@@ -49,9 +58,18 @@ void TutorialScheduler::Schedule(const Cpu &cpu, const StatusWord &agent_sw) {
     if (req->Commit()) {
       // succeeded !! 
       next->SetState(TutorialTaskState::kRunning);
+      // cs.current == nullptr か cs.current == next のどちらかしか起こりえない。
+      cs.current = next;
     } else {
       std::cerr << "Failed to commit txn." << std::endl;
       std::cerr << "txn state: " << RunRequest::StateToString(req->state()) << std::endl;
+      // コミットに失敗した場合はもとの状態に戻す。
+      if (next->queued()) { // nextがrqに入っていた場合
+        cs.rq.PushFront(next);
+      } else { // nextが実行中だった場合
+        CHECK(next->running());
+        CHECK_EQ(next, cs.current);
+      }
     }
   } else {
     // next候補がなければCPUを明け渡す（CPUはIDLE状態に）
@@ -60,47 +78,74 @@ void TutorialScheduler::Schedule(const Cpu &cpu, const StatusWord &agent_sw) {
 }
 
 void TutorialScheduler::TaskNew(TutorialTask* task, const Message& msg) {
-  std::cout << msg << std::endl;
-    // メッセージのペイロード部分にTASK_NEWメッセージ固有の情報が格納されている。
+  // メッセージのペイロード部分にTASK_NEWメッセージ固有の情報が格納されている。
   const ghost_msg_payload_task_new* payload = 
     reinterpret_cast<const ghost_msg_payload_task_new *>(msg.payload());
   // taskのseqnumを更新する（必須）
   task->seqnum = msg.seqnum();
   
+  // taskを割り当てるCPUを選択する。
+  CHECK_EQ(task->GetCpu(), -1);
+  auto cpu = SelectCpu();
+  // taskをcpuに関連付ける。
+  // ここの処理が正常に終了すると、このtaskに関連するメッセージはそのCPUのメッセージキューに
+  // 発行されるようになる。
+  int status;
+  bool result = cpu_state(cpu).channel->AssociateTask(task->gtid, task->seqnum, &status);
+  if (!result) {
+    // TaskNewでのASSOC_QUEUEが失敗した場合は、いったんデフォルトキューに関連付けを戻しておく。
+    printf("[!] Failed to AssociateTask in TaskNew. status = 0x%08x", status);
+    cpu = cpus()[0];
+  }
+
+  task->SetCpu(cpu.id());
+  printf("[*] Task %d is associated with cpu %d\n", task->gtid.tid(), cpu.id());
+
+  auto &cs = cpu_state(cpu);
   // payload->runnableがtrueのときはそのタスクがすでに実行可能状態であるということ、
   // 逆にfalseのときはスリープ状態であることを意味する。
   if (payload->runnable) {
     task->SetState(TutorialTaskState::kQueued);
-    rq_.push_back(task);
+    cs.rq.PushBack(task);
+    cs.agent->Ping();
   }
 }
 
 // スリープ状態 -> 実行可能状態
 void TutorialScheduler::TaskRunnable(TutorialTask* task, const Message& msg) {
+  CHECK_EQ(task->GetCpu(), MyCpu());
+  auto &cs = cpu_state(MyCpu());
+
   CHECK(task->blocked());
 
   // 実行可能キューの最後尾にtaskを追加
   task->SetState(TutorialTaskState::kQueued);
-  rq_.push_back(task);
+  cs.rq.PushBack(task);
 }
 
 // 実行状態 -> スリープ状態
 void TutorialScheduler::TaskBlocked(TutorialTask* task, const Message& msg) {
-  CHECK(!rq_.empty());
-  CHECK_EQ(task, rq_.front()); // 現在実行状態のタスクはrq_の先頭にいるはず
+  CHECK_EQ(task->GetCpu(), MyCpu());
+  auto &cs = cpu_state(MyCpu());
+
+  CHECK_EQ(task, cs.current);
   CHECK(task->running());
 
+  // currentをnullptrに変更する
   task->SetState(TutorialTaskState::kBlocked);
-  rq_.pop_front();
+  cs.current = nullptr;
 }
 
 // 任意の状態 -> 他のスケジューリングクラス
 void TutorialScheduler::TaskDeparted(TutorialTask* task, const Message& msg) {
-  // rq_の中にtaskがある場合はrq_から取り除く
-  rq_.erase(std::remove(
-    rq_.begin(), 
-    rq_.end(), 
-    task), rq_.end());
+  CHECK_EQ(task->GetCpu(), MyCpu());
+  auto &cs = cpu_state(MyCpu());
+
+  if (task->running()) {
+    cs.current = nullptr;
+  } else if (task->queued()) {
+    CHECK(cs.rq.Erase(task));
+  }
 
   // タスクが占めるメモリ領域を解放する
   allocator()->FreeTask(task);
@@ -108,6 +153,8 @@ void TutorialScheduler::TaskDeparted(TutorialTask* task, const Message& msg) {
 
 // スリープ状態 -> タスクの終了
 void TutorialScheduler::TaskDead(TutorialTask* task, const Message& msg) {
+  CHECK_EQ(task->GetCpu(), MyCpu());
+
   CHECK(task->blocked());
 
   // taskが占めるメモリ領域を解放する
@@ -116,23 +163,29 @@ void TutorialScheduler::TaskDead(TutorialTask* task, const Message& msg) {
 
 // 実行状態 -> 実行可能状態
 void TutorialScheduler::TaskYield(TutorialTask* task, const Message& msg) {
-  CHECK(!rq_.empty());
-  CHECK_EQ(task, rq_.front());
+  CHECK_EQ(task->GetCpu(), MyCpu());
+  auto &cs = cpu_state(MyCpu());
+
+  CHECK_EQ(task, cs.current);
   CHECK(task->running());
 
-  // rq_の先頭のタスクをrq_の最後尾に移動する
+  // cs.currentをcs.rqの最後尾に移動する。
+  cs.current = nullptr;
   task->SetState(TutorialTaskState::kQueued);
-  rq_.pop_front();
-  rq_.push_back(task);
+  cs.rq.PushBack(task);
 }
 
 // 実行状態 ➙ 実行可能状態
 void TutorialScheduler::TaskPreempted(TutorialTask* task, const Message& msg) {
-  CHECK(!rq_.empty());
-  CHECK_EQ(task, rq_.front());
+  CHECK_EQ(task->GetCpu(), MyCpu());
+  auto &cs = cpu_state(MyCpu());
+
+  CHECK_EQ(task, cs.current);
   CHECK(task->running());
 
+  cs.current = nullptr;
   task->SetState(TutorialTaskState::kQueued);
+  cs.rq.PushFront(task);
 }
 
 } // namespace ghost
--- schedulers/tutorial/per_cpu/tut_scheduler.h
+++ schedulers/tutorial/per_cpu/tut_scheduler.h
@@ -3,7 +3,9 @@
 #include "lib/agent.h"
 #include "lib/ghost.h"
 #include "lib/scheduler.h"
+#include <cstdio>
 #include <deque>
+#include <mutex>
 
 
 namespace ghost {
@@ -36,8 +38,95 @@ struct TutorialTask : public Task<> {
   bool queued() { return state_ == TutorialTaskState::kQueued; }
   bool running() { return state_ == TutorialTaskState::kRunning; }
 
+  int GetCpu() { return cpu_; }
+  void SetCpu(int cpu) { cpu_ = cpu; }
+
  private:
+  int cpu_ = -1;
   TutorialTaskState state_ = TutorialTaskState::kBlocked;
+};
+
+// タスクの実行可能キューを実装する。
+// 基本的にはdeqeueをミューテックスでラップしたもの。
+class TutorialRq {
+ public:
+  TutorialRq() = default;
+  TutorialRq(const TutorialRq &) = delete; // コピーの削除
+  TutorialRq &operator=(TutorialRq &) = delete; // コピーの削除
+
+  // キューの最後尾にtaskを追加する。
+  void PushBack(TutorialTask *task) {
+    std::lock_guard<std::mutex> guard(mtx_);
+    rq_.push_back(task);
+  }
+
+  // キューの先頭にtaskを追加する。
+  void PushFront(TutorialTask *task) {
+    std::lock_guard<std::mutex> guard(mtx_);
+    rq_.push_front(task);
+  }
+
+  // キューの先頭を取り出し、その値を返す。
+  // もしキューが空であればnullptrを返す。
+  TutorialTask *PopFront() {
+    std::lock_guard<std::mutex> guard(mtx_);
+    if (rq_.empty())
+      return nullptr;
+    auto ret = rq_.front();
+    rq_.pop_front();
+    return ret;
+  }
+
+  // キューからtaskを削除する。
+  // 無事にtaskの削除に成功すればtrueを返す。
+  // もし、キューにtaskが存在していなければfalseを返す。
+  bool Erase(TutorialTask *task) {
+    std::lock_guard<std::mutex> guard(mtx_);
+    for (auto it = rq_.begin(); it < rq_.end(); it++) {
+      if (*it == task) {
+        rq_.erase(it);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // キューに含まれているタスクの数を返す。
+  size_t Size() {
+    std::lock_guard<std::mutex> guard(mtx_);
+    return rq_.size();
+  }
+
+  // キューが空か？
+  bool Empty() { return Size() == 0; }
+
+  // キューにtaskが含まれているか？
+  bool Has(TutorialTask *task) {
+    std::lock_guard<std::mutex> guard(mtx_);
+    for (auto it = rq_.begin(); it < rq_.end(); it++) {
+      if (*it == task) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+ private:
+  std::mutex mtx_;
+  std::deque<TutorialTask *> rq_;
+};
+
+// CPUごとに管理するデータをまとめたもの。
+//  - current: CPUを使用しているghOStタスクへのポインタを格納する。
+//             CPUを使用中のタスクがなければnullptrとなる。
+//  - agent:   agentスレッド。
+//  - channel: CPUに関連付けられたChannelのユニークポインタ。
+//  - rq:      実行可能キュー。
+struct TutorialCpuState {
+  TutorialTask *current = nullptr;
+  Agent *agent = nullptr;
+  std::unique_ptr<Channel> channel = nullptr;
+  TutorialRq rq;
 };
 
 class TutorialScheduler : public BasicDispatchScheduler<TutorialTask> {
@@ -48,12 +137,24 @@ public:
       : BasicDispatchScheduler(enclave, std::move(cpulist), std::move(allocator)) {
     // 管理対象CPUに対してそれぞれ必要な処理を行っていく。
     for (auto cpu: cpulist) {
+      TutorialCpuState &cs = cpu_state(cpu);
       // ① cpu専用のChannelを作成
-      channels_[cpu.id()] = enclave->MakeChannel(GHOST_MAX_QUEUE_ELEMS, 0, MachineTopology()->ToCpuList({cpu}));
+      cs.channel = enclave->MakeChannel(GHOST_MAX_QUEUE_ELEMS, 0, MachineTopology()->ToCpuList({cpu}));
       
       // ② 最初に作成したChannelをデフォルトのものとして設定する
       if (!default_channel_)
-        default_channel_ = channels_[cpu.id()].get();
+        default_channel_ = cs.channel.get();
+    }
+  }
+
+  // 初期化処理の一番最後に呼び出される関数。
+  // この時点でAgentの初期化はほぼ完了していると想定していい。
+  // この関数が終了すると、Enclave::Readyは初期化の完了をAgentに伝達する。
+  void EnclaveReady() final {
+    for (auto cpu: cpus()) {
+      TutorialCpuState &cs = cpu_state(cpu);
+      cs.agent = enclave()->GetAgent(cpu);
+      printf("cs.agent: %p\n", cs.agent);
     }
   }
  
@@ -62,7 +163,7 @@ public:
   // 実装必須
   Channel& GetDefaultChannel() final { return *default_channel_; }
   // 実装任意
-  Channel& GetAgentChannel(const Cpu &cpu) override { return *channels_[cpu.id()]; }
+  Channel& GetAgentChannel(const Cpu &cpu) override { return *cpu_state(cpu).channel; }
 
 protected:
   // コンパイルを通すために実装（中身は無）
@@ -75,13 +176,33 @@ protected:
   void TaskPreempted(TutorialTask* task, const Message& msg) final;
 
 private:
-  // Channel用メンバ変数
-  // このクラスがChannelのインスタンスのライフタイムを管理する
-  std::unique_ptr<Channel> channels_[MAX_CPUS];
+  // 新しいタスクが追加されたとき、そのタスクを割り振るCPUを選択する関数。
+  // 実装はかなり単純化していて、cpus()のなかをぐるぐる周る感じ。
+  const Cpu SelectCpu() {
+    static auto begin = cpus().begin();
+    static auto end = cpus().end();
+    static auto next = cpus().end();
+
+    if (end == next)
+      next = begin;
+    return next++;
+  }
+
+  // 現在実行中のCPUを返す。
+  // TutorialSchedulerはいろいろなスレッドから呼び出されるので、
+  // 今どのagentが実行しているのか、といった情報があるとありがたい。
+  int MyCpu() {
+    static thread_local int cpu = -1;
+    if (cpu < 0)
+      cpu = sched_getcpu();
+    return cpu;
+  }
+
+  TutorialCpuState &cpu_state(int cpu) { return cpu_states_[cpu]; }
+  TutorialCpuState &cpu_state(const Cpu &cpu) { return cpu_states_[cpu.id()]; }
+
+  TutorialCpuState cpu_states_[MAX_CPUS];
   Channel *default_channel_ = nullptr;
-  // 実行可能キュー
-  // このキューの先頭にあるタスクが次に実行状態となるorすでに現在実行中のタスク
-  std::deque<TutorialTask *> rq_;
 };
 
 inline std::unique_ptr<TutorialScheduler> TutorialMultiThreadedScheduler(Enclave *enclave, CpuList cpulist) {
